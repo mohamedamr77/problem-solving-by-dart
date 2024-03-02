489bdd04a4ae70b319f96f9c20597cfb077d3da3
mohamedamr77 <moamr947@gmail.com>
2024-03-02 14:19:34 +0200

    optinal fun

---
+++ untitled1/bin/untitled1.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+void main(){
+    print(calculteFinalPrice(100));
+}
+ double calculteFinalPrice(double price , [double? discount]){
+  discount??=0; //if the value null give discount value 0
+  return price-(price*discount/100);
+ }
+ // maxBetweenTwoNumber(int n1, int n2){
+  // return max(n1, n2);
+  // }
+  // minBetweenTwoNumber(int n1, int n2){
+  // return min(n1, n2);
+  // }
+ 
